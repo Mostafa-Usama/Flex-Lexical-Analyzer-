bfc93a1411876822bb979dc39f967072dcec5275
Mostafa Usama <105542711+Mostafa-Usama@users.noreply.github.com>
2022-12-28 08:05:58 +0200

    Add files via upload

---
+++ Flex/bin/test.c
@@ -0,0 +1,39 @@
+
+asssda
+int main ( )
+{
+aaa
+    /* 
+
+	/*comment*/
+
+ */
+    
+    /* First comment 
+       first comment—line two*/
+    /* Second comment */
+    
+    int sum ; // comment
+int s=0xAa2l;
+int s=02L;
+
+int s=0b0101L;
+float s=+2.5e+3f;
+float s=+2.5D;
+bool x=true;
+int n=null;
+char x='\u03a9';
+char ch='c';
+char x="\u0309";
+    char w_12hat___is_me122_ = "This is a string_____";
+    sum =  sum + i ;
+    }
+    
+    for(i=0l;i<=16.2f;i++){ /* do
+    * something 
+    * here
+    */
+    }
+    
+    return 0l ;
+}
